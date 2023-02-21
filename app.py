a4857a5d6a0a53e9bfb31b2219c0c0a6ef05548e
JosueBautista <alejandrojhb84@gmail.com>
2023-02-21 17:44:35 -0600

    dynamic list to every job

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, render_template
-from database import load_jobs_from_db
-from dotenv import load_dotenv
-load_dotenv()
+from database import load_jobs_from_db, load_job_from_db
+import re
 
 app = Flask(__name__)
 
@@ -11,7 +10,8 @@ def home_route():
     return render_template(
         'home.html', 
         jobs=jobs_list, 
-        company_name='My company name'
+        company_name='My company name',
+        title='Home'
         )
 
 @app.route("/home", methods=['GET', 'POST'])
@@ -22,6 +22,18 @@ def home():
 def list_jobs():
     return jsonify(
         load_jobs_from_db()
+        )
+
+@app.route('/job/<id>')
+def show_job(id: int):
+    job = load_job_from_db(id)
+    if not job:
+        return 'Not Found', 404
+    return render_template(
+        'job_page.html', 
+        job=job, 
+        title=job['title'],
+        company_name='My company name'
         )
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+load_dotenv()
 import os
 
 db_string = os.getenv('DB_STRING_CONN')
@@ -20,3 +22,12 @@ def load_jobs_from_db():
         for row in result.fetchall():
             jobs.append(row._asdict())
         return jobs
+    
+def load_job_from_db(id: int):
+    with engine.connect() as conn:
+        result = conn.execute(text('SELECT * FROM jobs WHERE id = ' + str(id)))
+        rows = result.fetchall()
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
